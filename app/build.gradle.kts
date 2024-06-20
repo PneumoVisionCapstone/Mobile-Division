520f1aead755900d31ba2e3534de6d8b586802e5
naufalfizbar <naufal@anakfajar.com>
2024-06-20 12:00:43 +0700

    mobile pneimovision

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,8 @@ dependencies {
     implementation ("jp.wasabeef:glide-transformations:4.3.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
+    implementation ("com.google.android.material:material:1.5.0")
+
 
 
 }
